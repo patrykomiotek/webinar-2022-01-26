fd4a48d203568d1c39c4a5eaf90d15b36e3692d3
Patryk Omiotek <patryk.omiotek@gmail.com>
2022-01-11 12:52:41 +0100

    displaying address

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from 'ethers';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [address, setAddress] = useState<string>("");
+
+  const loadData = async () => {
+    const provider = new ethers.providers.Web3Provider(
+      window.ethereum
+    );
+    // const provider = new ethers.providers.JsonRpcProvider();
+
+    const signer = provider.getSigner();
+
+    const blockNumber = await provider.getBlockNumber()
+    console.log('blockNumber: ', blockNumber);
+
+    // Get the balance of an account (by address or ENS name, if supported by network)
+    const balance = await provider.getBalance("0x68fC90731D45b2513b85f500584C486F3Ed0607d")
+    console.log("balance: ", balance);
+    // // { BigNumber: "2337132817842795605" }
+
+    // // Often you need to format the output to something more user-friendly,
+    // // such as in ether (instead of wei)
+    console.log(ethers.utils.formatEther(balance))
+    // // '2.337132817842795605'
+
+    // // If a user enters a string in an input field, you may need
+    // // to convert it from ether (as a string) to wei (as a BigNumber)
+    console.log(ethers.utils.parseEther("1.0"));
+    // // { BigNumber: "1000000000000000000" }
+
+    // console.log(provider.getBalance(signer.getAddress()));
+
+    // super istotne aby odczytać z metamask adresy
+    await provider.send("eth_requestAccounts", []);
+
+    const myAddress = await signer.getAddress();
+    console.log("my address: ", myAddress);
+
+    setAddress(myAddress);
+  }
+
+  useEffect(() => {
+    loadData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Address: {address}</h3>
     </div>
   );
 }
