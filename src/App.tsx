85bee58c2966ca8d1300b60d937ba1c47c3bfd26
Patryk Omiotek <patryk.omiotek@gmail.com>
2022-01-26 16:55:59 +0100

    sample connections

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { ethers } from 'ethers';
 import { useState, useEffect } from 'react';
+import abi from './abi.json';
 
 function App() {
 
@@ -22,6 +23,7 @@ function App() {
     // Get the balance of an account (by address or ENS name, if supported by network)
     const balance = await provider.getBalance("0x68fC90731D45b2513b85f500584C486F3Ed0607d")
     console.log("balance: ", balance);
+
     // // { BigNumber: "2337132817842795605" }
 
     // // Often you need to format the output to something more user-friendly,
@@ -37,12 +39,36 @@ function App() {
     // console.log(provider.getBalance(signer.getAddress()));
 
     // super istotne aby odczytać z metamask adresy
+    // to z dokumentacji MetaMask!!! https://docs.metamask.io/guide/rpc-api.html#permissions
+    // https://eth.wiki/json-rpc/API#eth_getbalance
     await provider.send("eth_requestAccounts", []);
 
+    // const myAddress = await signer.getAddress();
     const myAddress = await signer.getAddress();
     console.log("my address: ", myAddress);
 
     setAddress(myAddress);
+
+    // const resp = await provider.send("eth_getBalance", []);
+    // console.log('resp: ', resp);
+    const myBalance = await signer.getBalance();
+    console.log('myBalance: ', ethers.utils.formatEther(myBalance));
+
+    // Custom token
+    const erc20 = new ethers.Contract(
+      "0xC0133a3009c9EaA905BD95e901163AA4524374FD",
+      abi,
+      provider,
+    );
+
+    const erc20Balance = await erc20.balanceOf(myAddress);
+console.log("Contract: ", ethers.utils.formatEther(erc20Balance));
+
+    // const tx = signer.sendTransaction({
+    //   to: "ricmoo.firefly.eth",
+    //   value: ethers.utils.parseEther("1.0")
+    // });
+
   }
 
   useEffect(() => {
